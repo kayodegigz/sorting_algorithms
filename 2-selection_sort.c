b5ee507a16a1ee410f821c468fb06dee9677eaf8
kayodegigz <kayogunrinde@gmail.com>
2022-07-24 00:41:55 +0100

    insertion sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+void selection_sort(int *array, size_t size)
+{
+	size_t idx_min, current, next_idx;
+	int temp;
+
+	for(current = 0; current < size - 1; current++)
+	{
+		next_idx = current + 1;
+		idx_min = current;
+
+		while(next_idx < size)
+		{
+			if (array[next_idx] < array[idx_min])
+			{
+				idx_min = next_idx;
+			}
+			next_idx++;
+		}
+		if(array[idx_min] < array[current])
+		{
+			temp = array[idx_min];
+			array[idx_min] = array[current];
+			array[current] = temp;
+			print_array(array, size);
+		}
+	}
+}
