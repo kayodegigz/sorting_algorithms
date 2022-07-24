ece05a37c12a3e398376bfe2520245fca8797e37
Wasiu Sanusi <99918590+The-Vast@users.noreply.github.com>
2022-07-24 19:37:48 +0100

    More update on quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -27,16 +27,17 @@ void swap(int *a, int *b) {
  * Return: partition point
  */
 int partition(int *array, int low, int high) {
+  int pivot, i, j;
   
   /* select the rightmost element as pivot */
-  int pivot = array[high];
+  pivot = array[high];
   
   /* pointer for greater element */
-  int i = (low - 1);
+  i = (low - 1);
 
   /* traverse each element of the array */
   /* compare them with the pivot */
-  for (int j = low; j < high; j++) {
+  for (j = low; j < high; j++) {
     if (array[j] <= pivot) {
       /**
        * if element smaller than pivot is found 
@@ -74,15 +75,13 @@ void quick_sort(int *array, size_t size)
   
   if (low < high)
   {
-    // find the pivot element such that
-    // elements smaller than pivot are on left of pivot
-    // elements greater than pivot are on right of pivot
     int pi = partition(array, low, high);
+    high = pi - 1
+    /* recursive call on the left of pivot */
+    quick_sort(array, size);
     
-    // recursive call on the left of pivot
-    quickSort(array, low, pi - 1);
-    
-    // recursive call on the right of pivot
-    quickSort(array, pi + 1, high);
+    low = pi + 1
+    /* recursive call on the right of pivot */
+    quick_sort(array, size);
   }
 }
