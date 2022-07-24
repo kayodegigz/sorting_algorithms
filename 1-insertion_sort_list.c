fdc06d51ad4db06158a65cb6bd06140a3a69ddab
kayodegigz <kayogunrinde@gmail.com>
2022-07-24 01:20:47 +0100

    betty checks for insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 
 /**
- * bubble_sort - bubble sort algo that sorts elements in an array in asc order
+ * insertion_sort_list - insertion sort algo that sorts
+ * elements in a list in asc order
  *
  * @list: pointer to the pointer to the head/list to the sorted
  *
@@ -10,34 +11,35 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *head, *next, *previous, *current;
-    if(list == NULL || *list == NULL || (*list)->next == NULL)
-            return;
+	listint_t *head, *next, *previous, *current;
 
-    head = (*list);
-    current = (*list)->next;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    for(; current != NULL; current = next)
-    {
-        next = current->next;
-        previous = current->prev;
+	head = (*list);
+	current = (*list)->next;
 
-        while(previous && previous->n > current->n)
-        {
-            if(current->next != NULL)
-                current->next->prev = previous;
-            if(previous->prev != NULL)
-                previous->prev->next = current;
-            else
-                head = current;
-            previous->next = current->next;
-            current->prev = previous->prev;
-            current->next = previous;
-            previous->prev = current;
-            print_list(head);
+	for (; current != NULL; current = next)
+	{
+		next = current->next;
+		previous = current->prev;
 
-            previous = current->prev;
-        }
-    }
-    *list = head;
+		while (previous && previous->n > current->n)
+		{
+			if (current->next != NULL)
+				current->next->prev = previous;
+			if (previous->prev != NULL)
+				previous->prev->next = current;
+			else
+				head = current;
+			previous->next = current->next;
+			current->prev = previous->prev;
+			current->next = previous;
+			previous->prev = current;
+			print_list(head);
+
+			previous = current->prev;
+		}
+	}
+	*list = head;
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,15 +1,25 @@
 #include "sort.h"
+/**
+ * selection_sort - sorts elements in an array using selection algo
+ *
+ * @array: pointer to the first element of the array
+ *
+ * @size: number of elements in the array
+ *
+ * Return: returns nothing
+ */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t idx_min, current, next_idx;
 	int temp;
 
-	for(current = 0; current < size - 1; current++)
+	for (current = 0; current < size - 1; current++)
 	{
 		next_idx = current + 1;
 		idx_min = current;
 
-		while(next_idx < size)
+		while (next_idx < size)
 		{
 			if (array[next_idx] < array[idx_min])
 			{
@@ -17,7 +27,7 @@ void selection_sort(int *array, size_t size)
 			}
 			next_idx++;
 		}
-		if(array[idx_min] < array[current])
+		if (array[idx_min] < array[current])
 		{
 			temp = array[idx_min];
 			array[idx_min] = array[current];
